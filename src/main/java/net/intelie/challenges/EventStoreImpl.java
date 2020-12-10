312e71eb96675fe7ae53adbfbebeab94f5255c87
Irlan Cidade <icidade@gmail.com>
2020-12-10 20:52:34 -0300

    Work in progress

---
+++ src/main/java/net/intelie/challenges/EventStoreImpl.java
@@ -0,0 +1,38 @@
+package net.intelie.challenges;
+
+/**
+ * Implementation of the {@link EventStore} interface
+ * <p><br>TODO: write the documentation including the decisions made.</p>
+ * @author Irlan Cidade (icidade@gmail.com)
+ */
+public class EventStoreImpl implements EventStore{
+    /**
+     *
+     * @param event
+     */
+    @Override
+    public void insert(Event event) {
+        //TODO: implement
+    }
+
+    /**
+     *
+     * @param type
+     */
+    @Override
+    public void removeAll(String type) {
+        //TODO: implement
+    }
+
+    /**
+     *
+     * @param type      The type we are querying for.
+     * @param startTime Start timestamp (inclusive).
+     * @param endTime   End timestamp (exclusive).
+     * @return
+     */
+    @Override
+    public EventIterator query(String type, long startTime, long endTime) {
+        return null;  //TODO: implement
+    }
+}
